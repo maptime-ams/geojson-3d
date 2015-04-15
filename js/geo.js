147e6bf865faa4f31f9c9363a0776c8d9262ab69
Bert Spaan <bert.spaan@gmail.com>
2015-04-15 15:40:14 +0200

    Almost!

--- js/geo.js
+++ js/geo.js
@@ -1,16 +1,35 @@
+
+// return scaling factor that fit bounds within width/height
+function fit(bounds, width, height)
+{
+  var topLeft = bounds[0];
+  var bottomRight = bounds[1];
+
+  var w = bottomRight[0] - topLeft[0];
+  var h = bottomRight[1] - topLeft[1];
+
+  var hscale = width / w;
+  var vscale = height / h;
+
+  // pick the smallest scaling factor
+  var scale = (hscale < vscale) ? hscale : vscale;
+
+  return scale;
+}
+
 // D3.js projection from GeoJSON bounds
 function getProjection(geojson, width, height) {
   // From:
-  // http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object?answertab=active#tab-top
+  //   http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object?answertab=active#tab-top
   // We are using Turf to compute centroid (turf.centroid) and bounds (turf.envelope) because
   // D3's geo.centroid and path.bounds function expect clockwise polygons, which we cannot always guarantee:
-  // https://github.com/mbostock/d3/wiki/Geo-Paths#_path
+  //   https://github.com/mbostock/d3/wiki/Geo-Paths#_path
 
   var center = turf.centroid(geojson).geometry.coordinates;
-  var scale  = 150;
-  var offset = [width / 2, height / 2];
-  var projection = d3.geo.mercator().scale(scale).center(center)
-        .translate(offset);
+  var projection = d3.geo.mercator()
+        .center(center)
+        .scale(1)
+        .translate([0, 0]);
 
   // Create the path
   var path = d3.geo.path().projection(projection);
@@ -18,18 +37,16 @@ function getProjection(geojson, width, height) {
   // Using the path determine the bounds of the current map and use
   // these to determine better values for the scale and translation
 
-  var env = turf.envelope(geojson);
-  var bounds = path.bounds(env);
+  // var env = turf.envelope(geojson);
+  var bounds = path.bounds(geojson);
 
-  var hscale = scale * width  / (bounds[1][0] - bounds[0][0]);
-  var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
-  var scale = (hscale < vscale) ? hscale : vscale;
-  var offset = [0, 0];
+  var scale = fit(bounds, width, height);
 
   // New projection
-  projection = d3.geo.mercator().center(center)
-    .scale(scale * 150).translate(offset);
-  path = path.projection(projection);
+  projection = d3.geo.mercator()
+    .center(center)
+    .scale(scale)
+    .translate([0,0]);
 
   return projection;
 }
@@ -45,7 +62,6 @@ function ringToPoints(ring, projection) {
 // Create Three.js polygon from GeoJSON Polygon
 function createPolygonShape(polygon, projection) {
   var outerRing = polygon[0];
-  outerRing.reverse()
   var points = ringToPoints(outerRing, projection);
   var polygonShape = new THREE.Shape(points);
 
